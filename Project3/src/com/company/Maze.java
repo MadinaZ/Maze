757a87aee7420b685249119ed4f5e0f2224e781a
Darwish Quiambao <darwishjeric.quiambao@sjsu.edu>
2021-05-05 20:14:13 -0700

    JUnit Tests written

--- Project3/src/com/company/Maze.java
+++ Project3/src/com/company/Maze.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.File;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Maze {
@@ -7,14 +9,26 @@ public class Maze {
     Cell currentCell;
     int totalCell;
     int visitedCell;
-    Cell grid [][];
+    BFS bfs;
+    DFS dfs;
+    Cell[][] grid;
     Stack<Cell> stack = new Stack<>();
     private Random rand;
 
-
-
     public Maze(int gridSize) {
         this.gridSize = gridSize;
+        grid = new Cell[gridSize][gridSize];
+
+        for(int i = 0; i < gridSize; i++)
+            for(int j = 0; j < gridSize; j++)
+                grid[i][j] = new Cell(i, j);
+
+        grid[0][0].north = false;
+        grid[gridSize-1][gridSize-1].south = false;
+        createMaze();
+    }
+
+    public Maze(int gridSize, File fileIn){
         grid = new Cell[gridSize][gridSize];
 
         for(int i = 0; i < gridSize; i++)
@@ -64,8 +78,6 @@ public class Maze {
         return walls;
     }
 
-
-
     @Override
     public String toString() {
         String[][] chars = new String[gridSize*2 + 1][gridSize*2 + 1];
@@ -104,25 +116,47 @@ public class Maze {
             }
         }
 
-        for(int i = 0; i < chars.length; i++){
-            for(int j = 0; j < chars.length; j++)
-                s += chars[i][j];
+        for (String[] aChar : chars) {
+            for (int j = 0; j < chars.length; j++)
+                s += aChar[j];
             s += "\n";
         }
         return s;
     }
 
     public void solveDFS(){
-        DFS dfs = new DFS(gridSize, grid);
+        dfs = new DFS(gridSize, grid);
         dfs.Dfs();
         System.out.println(dfs.printDFS());
         System.out.println(dfs.printDFSShortestPath());
+
+        //Display Path coordinates, length of path, and cells visited.
+        System.out.print("Path: ");
+        dfs.getDfsVisit().forEach(cell -> System.out.print("(" + cell.x +"," + cell.y + ")"));
+        System.out.println("\nLength of Path: " + dfs.getPathLength());
+        System.out.println("Visited Cells: " + dfs.getCellsVisited());
+
     }
 
     public void solveBFS(){
-        BFS bfs = new BFS(gridSize, grid);
+        bfs = new BFS(gridSize, grid);
         bfs.bfs();
         System.out.println(bfs.printBFS());
         System.out.println(bfs.printBFSShortestPath());
+        //To properly display the arrayList in reverse (since queue is used to add into array list the coordinates)
+        Stack<String> bfsStack = new Stack<>();
+
+        //Traverse the arraylist and push into stack
+        bfs.getBfsVisit().forEach(cell -> bfsStack.push("("+cell.x +","+ cell.y+")"));
+
+        //bfs.getBfsVisit().forEach(cell -> System.out.print("("+cell.x +","+ cell.y+")"));
+        System.out.print("Path: ");
+        //pop out stack.
+        while(!bfsStack.isEmpty()){
+            System.out.print(bfsStack.pop());
+        }
+        System.out.println("\nLength of Path: " + bfs.getPathLength());
+        System.out.println("Visited Cells: " + bfs.getCellsVisited());
     }
+
 }
--- Project3/src/com/company/MazeTest.java
+++ Project3/src/com/company/MazeTest.java
@@ -1,4 +1,125 @@
 package com.company;
 
+import jdk.jfr.Description;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+@Testable
 public class MazeTest {
+    Maze maze;
+    MazeTest(){
+        Random rand = new Random();
+        //rand.setSeed();
+        maze = new Maze(6);
+    }
+
+    @Description("For use with incrementalSize_Test")
+    private void generateTestMaze(int i) {
+        Maze maze = new Maze(i * 2);
+        System.out.println("Grid Size: " + i * 2 + "\nOriginal maze: ");
+        System.out.println(maze);
+
+        System.out.println("DFS: ");
+        double startTime = Math.ceil(System.nanoTime());
+        maze.solveDFS();
+        double endTime = Math.ceil(System.nanoTime());
+        System.out.println("Time (ms)" + ((endTime - startTime) / 1000000));
+
+        System.out.println("\nBFS: ");
+        startTime = Math.ceil(System.nanoTime());
+        maze.solveBFS();
+        endTime = Math.ceil(System.nanoTime());
+        System.out.println("Time (ms)" + ((endTime - startTime) / 1000000));
+        System.out.println("");
+        Assert.assertEquals(maze.bfs.printBFSShortestPath(), maze.dfs.printDFSShortestPath());
+    }
+
+    @Test
+    @Description("Loops through grid sizes of 2 to 20 to construct random mazes and be solved")
+    public void incrementalSize_Test() {
+        //Check for random Mazes of Sizes 2 to 20.
+        for (int i = 1; i <= 10; i++) {
+            if (i <= 5) {
+                generateTestMaze(i);
+            } else if (i == 10) {
+                generateTestMaze(i);
+            }
+        }
+    }
+
+    @Test
+    @Description("Writes BFS (size 6) solved maze into file and compares it")
+    public void BFS_6_writeFile_Test() {
+        try {
+
+            String fileName = "maze6BFS.txt";
+            PrintWriter print = new PrintWriter(fileName);
+            System.out.println("Maze of Size 6 BFS: ");
+            this.maze.solveBFS();
+
+            String BFS = this.maze.bfs.printBFSShortestPath();
+
+            helper_write_to_file(fileName, print, BFS);
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+    @Test
+    @Description("Writes BFS (size 6) solved maze into file and compares it")
+    public void DFS_6_writeFile_Test() {
+        try {
+            String fileName = "maze6DFS.txt";
+            PrintWriter print = new PrintWriter(fileName);
+            System.out.println("Maze of Size 6 DFS: ");
+            this.maze.solveDFS();
+
+            String DFS = this.maze.dfs.printDFSShortestPath();
+
+            helper_write_to_file(fileName, print, DFS);
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    @Description("Helper for DFS and BFS write to file tests.")
+    private void helper_write_to_file(String fileName, PrintWriter fileWriter, String traversalType) throws FileNotFoundException {
+        fileWriter.write(traversalType);
+        fileWriter.close();
+        Scanner reader = new Scanner(new File(fileName));
+        StringBuilder readFromFile = new StringBuilder();
+        while(reader.hasNextLine()){
+            readFromFile.append(reader.nextLine()).append("\n");
+        }
+        reader.close();
+
+        Assert.assertEquals(traversalType, readFromFile.toString());
+
+    }
+
+    @Test
+    @Description("Tests whether DFS and BFS shortest path from constructed maze is the same")
+    public void DFS_BFS_Equals()
+    {
+        maze.solveDFS();
+        maze.solveBFS();
+        Assert.assertEquals(this.maze.dfs.printDFSShortestPath(), this.maze.bfs.printBFSShortestPath());
+    }
+
+    @Test
+    @Description("Tests whether all cells have at least one wall broken")
+    public void cellWalls_Test(){
+        for(int i = 0; i <maze.gridSize - 1; i++){
+            for(int j = 1; j < maze.gridSize - 1; j++)
+            {
+                Assert.assertFalse(maze.grid[i][j].allWallsIntact());
+            }
+        }
+    }
 }
